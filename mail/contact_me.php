3e99e05e1ddbc9720a32146326d2e286c622bac4
Joe Vogel <joejvogel@gmail.com>
2018-10-10 16:46:56 -0300

    Limpeza para deixar apenas o site

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
@@ -11,10 +12,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "joejvogel@gmail.com";
+$to = "julianavaroni@gmail.com";
 $subject = "Novo contato recebido:  $name";
 $body = "Você recebeu uma nova mensagem do site Varoni.\n\n"."Estes são os detalhes do contato:\n\Nome: $name\n\nE-mail: $email\n\nFone: $phone\n\nMensagem:\n$message";
-$header = "From: noreply@varoni.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: nao_responda@varoniacessorios.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
